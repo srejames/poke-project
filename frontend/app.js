8d7404e2f3b62e6fbd82bc33333a6ce7a4610eda
srejames <ethan.hidalgo@gmail.com>
2026-05-28 14:01:32 -0700

    added initial HTML webpage and searchbar, created dummy card data

---
+++ frontend/app.js
@@ -0,0 +1,30 @@
+// constants
+
+const cards = [
+    {
+        name: "Charizard",
+        set: "Evolutions",
+        rarity: "Holofoil",
+        price: 95.50,
+        trend: "+10.1%",
+        image: "https://tcgplayer-cdn.tcgplayer.com/product/124026_in_1000x1000.jpg"
+    },
+    {
+        name: "Pikachu",
+        set: "Generations (Radiant Collection)",
+        rarity: "Full Art",
+        price: 127.42,
+        trend: "-6.1%",
+        image: "https://tcgplayer-cdn.tcgplayer.com/product/113771_in_1000x1000.jpg"
+    }
+];
+
+const cardContainer = document.getElementById("card-container");
+
+// logic
+
+cards.forEach(card => {
+    const cardHTML = `
+        
+    `;
+});
